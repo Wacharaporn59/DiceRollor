6d646583da2c76fd0c8773f1f1f3a8d8be55ad04
Wacharaporn59 <59160130@go.buu.ac.th>
2019-09-15 21:02:53 +0700

    homework

--- app/src/main/java/buu/informatics/s59160130/diceroller/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160130/diceroller/MainActivity.kt
@@ -18,6 +18,9 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{ rollDice()}
 
+        val resetButton: Button = findViewById(R.id.reset_button)
+        resetButton.setOnClickListener{ resetDice()}
+
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = "Dice_Rolled!"
 
@@ -29,6 +32,13 @@ class MainActivity : AppCompatActivity() {
 
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun resetDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "0".toString()
 
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
     }
